ea482d8c846ac97f682d9a06794f1aef35eecc9f
王成阳 <985341454@qq.com>
2021-06-15 15:42:20 +0800

    blogContent 2021/06/15 周二

--- 编译.bat
+++ 编译.bat
@@ -1,1 +1,15 @@
+:: 构建静态内容
 hugo -d ../damuwangs.github.io
+
+:: 提交blogContent
+git add .
+git commit -m "blogContent %date%" 
+git push origin main
+
+:: 切换到damuwangs.github.io文件夹
+cd ../damuwangs.github.io
+git add .
+git commit -m "damuwangs.github.io %date%" 
+git push origin main
+
+pause
